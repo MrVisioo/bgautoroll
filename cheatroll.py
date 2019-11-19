852d4fb1fd01fadf50eb1d8f5fc713aed5940145
Sergey Aslamazov <35179551+MrVisioo@users.noreply.github.com>
2019-11-19 12:31:43 +0300

    initial

---
+++ cheatroll.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageOps
+import pyautogui
+import tesserocr
+from tesserocr import PyTessBaseAPI, PSM
+import sys
+
+score = int(sys.argv[1])
+rolls = int(sys.argv[2])
+with PyTessBaseAPI(psm=PSM.SINGLE_WORD) as api:
+  for i in range(rolls:
+    shot = pyautogui.screenshot(region=(462,797,143,30))
+    api.SetImage(shot)
+    if int(api.GetUTF8Text()) < score:
+      pyautogui.click(x=600, y=900)
+    else:
+     break 
